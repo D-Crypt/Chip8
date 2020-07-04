089c321a4e23bd1507141f99ea82680e61e69469
D-Crypt <danielvaughan54@gmail.com>
2020-07-04 10:58:00 +0100

    Complete opcode declarations

--- Chip8.h
+++ Chip8.h
@@ -36,11 +36,30 @@ public:
 	void op_6XKK(); // Set VX = KK
 	void op_7XKK(); // Set VX += KK
 	void op_8XY0(); // Set VX = VY
-	void op_8XY1(); // Set VX = VX || VY
-	void op_8XY2(); // Set VX = VX && VY
+	void op_8XY1(); // Set VX = VX | VY
+	void op_8XY2(); // Set VX = VX & VY
 	void op_8XY3(); // Set VX = VX ^ (XOR) VY
 	void op_8XY4(); // Set VX += VY, set VF = carry
 	void op_8XY5(); // Set VX -= VY, set VF = NOT borrow
-	void op_8XY6(); // 
+	void op_8XY6(); // Set VX = VX SHR 1
+	void op_8XY7(); // Set VX = VY - VX, set VF = NOT borrow
+	void op_8XYE(); // Set VX = VX SHL 1
+	void op_9XY0(); // Skip next instruction if VX != VY
+	void op_ANNN(); // Set I = NNN
+	void op_BNNN(); // Jump to location NNN + V0
+	void op_CXKK(); // Set VX = random byte AND KK
+	void op_DXYN(); // Display n-byte sprite starting at memory location I at (VX, VY), set VF = collision
+	void op_EX9E(); // Skip next instruction if key with the value of VX is pressed
+	void op_EXA1(); // Skip next instruction if key with the value of VX is not pressed
+	void op_FX07(); // Set VX = delay timer value
+	void op_FX0A(); // Wait for a key press, store the value of the key in VX
+	void op_FX15(); // Set delay timer = VX
+	void op_FX18(); // Set sound timer = VX
+	void op_FX1E(); // Set I += VX
+	void op_FX29(); // Set I = location of sprite for digit VX
+	void op_FX33(); // Store BCD representation of VX in memory locations I, I+1 and I+2
+	void op_FX55(); // Store registers V0 through VX in memory starting at location I
+	void op_FX65(); // Read registers V0 through VX from memory starting at location I
+
 };
 
