97347a42009306466164b8c128edbacd5afa37a3
D-Crypt <danielvaughan54@gmail.com>
2020-07-07 13:03:47 +0100

    Define opTable function

--- Chip8.cpp
+++ Chip8.cpp
@@ -399,3 +399,104 @@ void Chip8::op_FX65()
 		registers[i] = memory[index + i];
 	}
 }
+
+void Chip8::opTable()
+{
+	switch (opcode & 0xF000)
+	{
+	case 0x0000:
+		switch (opcode)
+		{
+		case 0x00E0:
+			op_00E0();
+			break;
+		case 0x00EE:
+			op_00EE();
+			break;
+		}
+		
+		break;
+	case 0x1000:
+		break;
+	case 0x2000:
+		break;
+	case 0x3000:
+		break;
+	case 0x4000:
+		break;
+	case 0x5000:
+		break;
+	case 0x6000:
+		break;
+	case 0x7000:
+		break;
+	case 0x8000:
+		switch (opcode & 0xF)
+		{
+		case 0x0:
+			break;
+		case 0x1:
+			break;
+		case 0x2:
+			break;
+		case 0x3:
+			break;
+		case 0x4:
+			break;
+		case 0x5:
+			break;
+		case 0x6:
+			break;
+		case 0x7:
+			break;
+		case 0xE:
+			break;
+		}
+
+		break;
+	case 0x9000:
+		break;
+	case 0xA000:
+		break;
+	case 0xB000:
+		break;
+	case 0xC000:
+		break;
+	case 0xD000:
+		break;
+	case 0xE000:
+		switch (opcode & 0xFF)
+		{
+		case 0x9E:
+			break;
+		case 0xA1:
+			break;
+		}
+
+		break;
+	case 0xF000:
+		switch (opcode & 0xFF)
+		{
+		case 0x07:
+			break;
+		case 0x0A:
+			break;
+		case 0x15:
+			break;
+		case 0x18:
+			break;
+		case 0x1E:
+			break;
+		case 0x29:
+			break;
+		case 0x33:
+			break;
+		case 0x55:
+			break;
+		case 0x65:
+			break;
+		}
+
+		break;
+	}
+}
