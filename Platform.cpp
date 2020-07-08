068da3a7494fbc59ba74f939f15ed3d3f3c521dd
D-Crypt <danielvaughan54@gmail.com>
2020-07-08 12:57:39 +0100

    Define Platform constructor

--- Platform.cpp
+++ Platform.cpp
@@ -1,1 +1,10 @@
 #include "Platform.h"
+
+Platform::Platform(char const* title, int windowWidth, int windowHeight, int textureWidth, int textureHeight)
+{
+	SDL_Init(SDL_INIT_VIDEO);
+
+	window = SDL_CreateWindow(title, 0, 0, windowWidth, windowHeight, SDL_WINDOW_SHOWN);
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, textureWidth, textureHeight);
+}
